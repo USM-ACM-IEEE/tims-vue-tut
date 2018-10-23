5b409590e3ac90ff66a9ca480ccf6a3c43628baa
tctcodes <timthom64@gmail.com>
2018-10-23 14:03:34 -0500

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app id="app">
     <router-view/>
-  </div>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
   router,
