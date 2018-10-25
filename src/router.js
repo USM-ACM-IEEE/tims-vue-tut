f2a543e4ba254ed02d4ba391d1f87115095f4e8d
tctcodes <timthom64@gmail.com>
2018-10-25 00:45:29 -0500

    starter code

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Todo from '@/components/TodoList.vue';
 
 Vue.use(Router);
 
@@ -8,10 +7,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'Todo',
-      component: Todo,
-    },
+    {},
   ],
 });
